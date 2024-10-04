9bfb41e3e072b767a5ca05f143212ab20d7a5aa2
Lorenzo <lorenzo1324enzo@gmail.com>
2024-10-03 21:58:48 -0300

    Aula 03/10/2024

--- mater-play-backend/src/app.module.ts
+++ mater-play-backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ mater-play-backend/src/entities/movie-entity.ts
@@ -0,0 +1,8 @@
+export class Movie {
+    id: string;
+    title: string;
+    description: string;
+    ageRating: string;
+    poster: string;
+
+}
